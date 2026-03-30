c15fa6dab3fa0496872e237e68627f3d040a48b2
HarshRaut9945 <harshraut9945@gmail.com>
2026-03-31 00:48:35 +0530

    add

---
+++ frontend/src/home/RightPart/ChatUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatUser = () => {
+  return (
+    <div>
+      ChatUser
+    </div>
+  )
+}
+
+export default ChatUser
--- frontend/src/home/RightPart/Right.jsx
+++ frontend/src/home/RightPart/Right.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ChatUser from './ChatUser'
 
 const Right = () => {
   return (
     <div className='w-[70%] border bg-slate-900'>
-      Right
+      <ChatUser/>
     </div>
   )
 }
