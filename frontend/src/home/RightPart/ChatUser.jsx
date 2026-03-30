3f5c6a5a3839517000fc73c5647d705aef5a34ca
HarshRaut9945 <harshraut9945@gmail.com>
2026-03-31 01:01:53 +0530

    add

--- frontend/src/home/RightPart/ChatUser.jsx
+++ frontend/src/home/RightPart/ChatUser.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const ChatUser = () => {
-  return (
-    <div>
-      ChatUser
-    </div>
-  )
+    return (
+        <div className="flex items-center justify-center gap-4 p-4 border-b border-gray-200 dark:border-gray-700">
+            <div className="avatar avatar-online">
+                <div className="w-12 rounded-full ring ring-primary ring-offset-2">
+                    <img 
+                        src="https://img.daisyui.com/images/profile/demo/gordon@192.webp" 
+                        alt="User avatar"
+                    />
+                </div>
+            </div>
+            <div className="flex-1">
+                <h1 className="font-bold text-lg text-gray-900 dark:text-white">Harsh R</h1>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Offline</span>
+            </div>
+        </div>
+    )
 }
 
 export default ChatUser
--- frontend/src/home/RightPart/Right.jsx
+++ frontend/src/home/RightPart/Right.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChatUser from './ChatUser'
+import Message from './Message'
 
 const Right = () => {
   return (
     <div className='w-[70%] border bg-slate-900'>
       <ChatUser/>
+      <Message/>
     </div>
   )
 }
